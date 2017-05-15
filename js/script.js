26ccf8a0eb1aa2e9045d4de3cae8d8e758b75c99
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-15 18:00:55 -0400

    initial json code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 //JS and jQuery for RQ
 $('document').ready(function(){
-
+	
 
 });
+
+
+$('#search-form').submit(function(){
+	var searchText = $('#search-input').val();
+	// alert(searchText);
+	retrieveSearchResults(searchText);
+});
+
+
+
+var retrieveSearchResults = function(searchText){
+	alert('from retrieveSearchResults');
+	var rootURL = 'https://en.wikipedia.org/w/api.php?';
+	var format = 'format=json';
+	var action = 'action=opensearch';
+	var search = 'search=' + searchText;
+	var limit = 'limit=10';
+
+	var requestURL = rootURL + action + '&' + format + '&' + search + '&' + limit;
+	alert(requestURL);
+
+	$.getJSON(requestURL, function(json){
+		var myStr = json;
+		alert(myStr);
+	});
+}
