385879f950170c2236e3f96b192278f0a2c9265d
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-16 10:58:14 -0400

    JSON get is working, being loaded into HTML of page, just have to parse results

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ $('document').ready(function(){
 });
 
 
-$('#search-form').submit(function(){
+$('#search-form').submit(function(e){
+	e.preventDefault();
 	var searchText = $('#search-input').val();
 	// alert(searchText);
 	retrieveSearchResults(searchText);
@@ -14,18 +15,18 @@ $('#search-form').submit(function(){
 
 
 var retrieveSearchResults = function(searchText){
-	alert('from retrieveSearchResults');
 	var rootURL = 'https://en.wikipedia.org/w/api.php?';
 	var format = 'format=json';
-	var action = 'action=opensearch';
-	var search = 'search=' + searchText;
+	var action = 'action=query';
+	var list = "list=search";
+	var search = 'srsearch=' + searchText;
 	var limit = 'limit=10';
 
-	var requestURL = rootURL + action + '&' + format + '&' + search + '&' + limit;
-	alert(requestURL);
+	var requestURL = rootURL + action + '&' + format + '&' + list + '&' + search + '&' + limit + '&callback=?';
 
 	$.getJSON(requestURL, function(json){
-		var myStr = json;
-		alert(myStr);
+		var myStr = JSON.stringify(json);
+		var resultsObj = JSON.parse(myStr);
+		$('#results-div-1').html(myStr);
 	});
 }
